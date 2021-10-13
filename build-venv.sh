7367ab858a1067f8f78932065856335b7dedda2e
Darshaka Pathirana <dpat@syn-net.org>
2021-10-13 13:18:26 +0200

    Make virtual environment base customizable

--- build-venv.sh
+++ build-venv.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-virtualenv "/opt/pydio-sync-venv"
-source "/opt/pydio-sync-venv/bin/activate"
+VENV_BASE="$1"
+
+virtualenv "${VENV_BASE}/pydio-sync-venv"
+source "${VENV_BASE}/pydio-sync-venv/bin/activate"
 cd /root/pydio-sync
 pip install -r src/pydio/sdkremote/requirements.txt
 pip install -r requirements.txt
